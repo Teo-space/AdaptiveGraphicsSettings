ba0e9fc9acafccba09250ac910788615ddb2876a
teo <62513275+Teo-space@users.noreply.github.com>
2020-05-28 20:42:11 +0300

    Add files via upload

---
+++ AdaptiveGraphicsSettings/AdaptiveGraphicsSettings.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using MelonLoader;
+using Microsoft.Win32;
+using UnityEngine;
+
+
+namespace AdaptiveGraphicsSettings
+{
+    public static class BuildInfo
+    {
+        public const string Name = "AdaptiveGraphicsSettings";
+        public const string Author = "Teo";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class AdaptiveGraphicsSettings : MelonMod
+    {
+        public static float Average = 0f;
+        public static float FPS = 0f;
+        private static float RefreshRate = 0f;
+        public override void OnUpdate()
+        {
+            //MelonModLogger.Log("OnUpdate");
+
+            Average += ((Time.deltaTime / Time.timeScale) - Average) * 0.03f;
+            FPS = 1 / Average;
+
+            RefreshRate += Time.deltaTime;
+            if (RefreshRate < 8f) return;
+            RefreshRate = 0f;
+
+            if (FPS > 50f)
+            {
+                ultra();
+            }
+            else if (FPS > 40f)
+            {
+                high();
+            }
+            else if (FPS > 30f)
+            {
+                medium();
+            }
+            else if (FPS > 20f)
+            {
+                low();
+            }
+            else if (FPS < 10f)
+            {
+                shit();
+            }
+            owo++;
+        }
+        int owo = 0;
+
+        public static void ultra()
+        {
+            QualitySettings.antiAliasing = 8;
+            QualitySettings.pixelLightCount = 8;
+            QualitySettings.lodBias = 2f;
+            QualitySettings.shadowDistance = 200f;
+        }
+        public static void high()
+        {
+            QualitySettings.antiAliasing = 6;
+            QualitySettings.pixelLightCount = 6;
+            QualitySettings.lodBias = 1.5f;
+            QualitySettings.shadowDistance = 180f;
+        }
+
+        public static void medium()
+        {
+            QualitySettings.antiAliasing = 4;
+            QualitySettings.pixelLightCount = 4;
+            QualitySettings.lodBias = 1.2f;
+            QualitySettings.shadowDistance = 120f;
+        }
+
+        public static void low()
+        {
+            QualitySettings.antiAliasing = 2;
+            QualitySettings.pixelLightCount = 2;
+            QualitySettings.lodBias = 0.8f;
+            QualitySettings.shadowDistance = 80f;
+        }
+
+        public static void shit()
+        {
+            QualitySettings.antiAliasing = 0;
+            QualitySettings.pixelLightCount = 0;
+            QualitySettings.lodBias = 0.4f;
+            QualitySettings.shadowDistance = 20f;
+        }
+
+
+    }
+}
---
+++ AdaptiveGraphicsSettings/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(AdaptiveGraphicsSettings.BuildInfo.Name)]
+[assembly: AssemblyDescription("Adaptive Graphics Settings for VRChat")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AdaptiveGraphicsSettings.BuildInfo.Company)]
+[assembly: AssemblyProduct(AdaptiveGraphicsSettings.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + AdaptiveGraphicsSettings.BuildInfo.Author)]
+[assembly: AssemblyTrademark(AdaptiveGraphicsSettings.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(AdaptiveGraphicsSettings.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AdaptiveGraphicsSettings.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(AdaptiveGraphicsSettings.AdaptiveGraphicsSettings), AdaptiveGraphicsSettings.BuildInfo.Name, AdaptiveGraphicsSettings.BuildInfo.Version, AdaptiveGraphicsSettings.BuildInfo.Author, AdaptiveGraphicsSettings.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonModGame(null, null)]
